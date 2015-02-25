016d7f901542550667d9591f1c2d2274ed546640
Tasos Deligiannis <spirtasos@gmail.com>
2015-02-25 13:13:46 +0000

    Updated capture_image.py

--- Image Processing/Testbench/capture_image.py
+++ Image Processing/Testbench/capture_image.py
@@ -1,18 +1,24 @@
 #! usr/bin/env python
 
-######################################################################
+##########################################################################
 # Test script to capture an image using the pi camera module.
 #
-# Output image folder: pi/Desktop/Image Processing/Testbench/Images/
+# Output image folder: pi/Desktop/tiberius/Image Processing/Testbench/Images/
 #
-# Date: 20/01/2015 
+# Date: 17/02/2015 
 # Version: 0.0
-######################################################################
+##########################################################################
 
 import picamera
+from time import sleep
 
-# Instantiate the camera module and capture an image.
+# Instantiate the camera module.
 camera = picamera.PiCamera()
 
-camera.capture('Images/test.jpg')
+# Display a preview to ensure that you capture the right test image.
+camera.start_preview()
+sleep(10)
+camera.stop_preview()
+
+camera.capture('Images/Test1/Trapezoid.jpg')
 
