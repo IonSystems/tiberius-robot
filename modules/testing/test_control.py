86b1886086dc94a57f65f866ff74811a6043f0c9
camieac <camieac@gmail.com>
2015-09-21 18:31:11 +0100

    Skip control tests if not on Pi. Added move backwards into wall test.

--- modules/testing/test_control.py
+++ modules/testing/test_control.py
@@ -1,22 +1,45 @@
 import unittest
 import time
-
+import os
 from control.control import Control
 
 c = Control()
+
+
+
 class DriveForwardUntilWall(unittest.TestCase):
 	'''Drive forward in a straight line until an obsacle is detected from the front ultrasonic sensor.'''
+	@unittest.skipUnless(os.uname()[4].startsWith("arm") , "requires Raspberry Pi")
 	def runTest(self):
+		if not is_raspberry_pi():
+			self.skip
 		time = 0
-		#Wait until Tiberius is 5cm away from te wall.
+		#Wait until Tiberius is 5cm away from the wall.
 		while(c.senseUltrasonic['fc'] > 5):
 			c.moveForward()
 			sleep(1)
 			time += 1
-			
+
 		#Cannot take any less than 3 seconds
 		self.assertGreater(time, 3)
+
+		#Cannot take any more than 30 seconds
+		self.assertLess(time, 30)
 		
+class DriveBackwardUntilWall(unittest.TestCase):
+	'''Drive backward in a straight line until an obsacle is detected from the front ultrasonic sensor.'''
+	@unittest.skipUnless(os.uname()[4].startsWith("arm") , "requires Raspberry Pi")
+	def runTest(self):
+		time = 0
+		#Wait until Tiberius is 5cm away from the wall.
+		while(c.senseUltrasonic['rc'] > 5):
+			c.moveBackward()
+			sleep(1)
+			time += 1
+
+		#Cannot take any less than 3 seconds
+		self.assertGreater(time, 3)
+
 		#Cannot take any more than 30 seconds
 		self.assertLess(time, 30)
 
