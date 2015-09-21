fcf177f628e0f2440323c452f370093a6a5fc86e
camieac <camieac@gmail.com>
2015-09-21 17:25:53 +0100

    Started refactoring control code, added smbus to vendor folder.

---
+++ modules/control/control.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import md03
+import srf08
+
+class Control:
+	'''Control the motor drivers'''
+	
+	#Front Right
+	srffr = srf08.UltrasonicRangefinder(0x72)
+	#Front Centre
+	srffc = srf08.UltrasonicRangefinder(0x71)
+	#Front Left
+	srffl = srf08.UltrasonicRangefinder(0x70)
+	#Rear Right
+	srfrr = srf08.UltrasonicRangefinder(0x73)
+	#Rear Centre
+	srfrc = srf08.UltrasonicRangefinder(0x74)
+	#Rear Left
+	srfrl = srf08.UltrasonicRangefinder(0x75)
+	
+	leftf = md03.MotorDriver(0x58)
+	leftr = md03.MotorDriver(0x5A)
+	rightf = md03.MotorDriver(0x5B)
+	rightr = md03.MotorDriver(0x59)
+
+	# 0 - 255
+	speed = 255
+	accel = 0
+	
+	def stop(self):
+		self.leftf.move(0, self.accel)
+		self.leftr.move(0, self.accel)
+		self.rightf.move(0, self.accel)
+		self.rightr.move(0, self.accel)
+		
+	def moveForward(self):
+		self.leftf.move(-self.speed, self.accel)
+        self.leftr.move(-self.speed, self.accel)
+        self.rightf.move(-self.speed, self.accel)
+        self.rightr.move(-self.speed, self.accel)
+
+	def moveBackward(self):
+		self.leftf.move(self.speed, self.accel)
+		self.leftr.move(self.speed, self.accel)
+		self.rightf.move(self.speed, self.accel)
+		self.rightr.move(self.speed, self.accel)
+		
+	#Turn on the spot, to the right
+	def turnRight(self):
+		self.leftf.move(-self.speed, self.accel)
+		self.leftr.move(-self.speed, self.accel)
+		self.rightf.move(self.speed, self.accel)
+		self.rightr.move(self.speed, self.accel)
+		
+	#Turn on the spot, to the left
+	def turnLeft(self):
+		self.leftf.move(self.speed, self.accel)
+		self.leftr.move(speed, self.accel)
+		self.rightf.move(-self.speed, self.accel)
+		self.rightr.move(-self.speed, self.accel)
+		
+	def senseUltrasonic(self):
+		#Tell sensors to write data to it's memory
+		self.srfrr.doranging()
+		
+		# Read the data from sensor's memory
+		self.srffrm = srffr.getranging()
+		self.srffcm = srffc.getranging()
+		self.srfflm = srffl.getranging()
+		self.srfrrm = srfrr.getranging()
+		self.srfrcm = srfrc.getranging()
+		self.srfrlm = srfrl.getranging()
---
+++ modules/control/srf08.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import smbus
+import time
+
+class UltrasonicRangefinder:
+	'''Ultrasonic rangefinder'''
+
+    # parameters
+    commandreg = 0x00
+    gainreg = 0x01
+    rangereg = 0x02
+    cm_mode = 0x51
+    first_echo_low = 0x03
+    first_echo_high = 0x02
+    range_value = 140
+    gain_value = 3
+    value = 0.0
+
+    def __init__(self, address):
+        self.bus = smbus.SMBus(1)
+        self.address = address
+        try:
+            self.bus.write_byte_data(self.address, self.rangereg, self.range_value)
+            self.bus.write_byte_data(self.address, self.gainreg, self.gain_value)
+        except IOError:
+            print 'IO error init ', hex(self.address)
+
+    def doranging(self):
+        try:
+            #since it takes 65ms to get the ranging and us to send a message
+            #in order to save time all the srf08s receive command to start
+            #ranging, so only one 65ms delay needed
+            self.bus.write_byte_data(0x70, self.commandreg, self.cm_mode)
+            self.bus.write_byte_data(0x71, self.commandreg, self.cm_mode)
+            self.bus.write_byte_data(0x72, self.commandreg, self.cm_mode)
+            self.bus.write_byte_data(0x73, self.commandreg, self.cm_mode)
+            self.bus.write_byte_data(0x74, self.commandreg, self.cm_mode)
+            self.bus.write_byte_data(0x75, self.commandreg, self.cm_mode)
+            time.sleep(0.065)
+        except IOError:
+            print 'IO error doranging'
+
+    def getranging(self):
+        try:
+            high_byte = self.bus.read_byte_data(self.address, self.first_echo_high)
+	    low_byte = self.bus.read_byte_data(self.address, self.first_echo_low)	    
+            if (((high_byte << 8) + low_byte)==0):
+                #assign a random value when srf08 failed to range
+                value = 222.2
+            else:
+                value = (high_byte << 8) + low_byte
+	    return value
+	
+	except IOError:
+            print 'IO error getranging ',hex(self.address)
+            return 222.2
+
