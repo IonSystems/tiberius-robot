37c87c7f4f400a545afdd21853f03bc3a0bcc98e
Cameron A. Craig <camieac@gmail.com>
2015-10-03 18:24:13 +0100

    Added bootstrap template.

--- modules/web-interface/control/views.py
+++ modules/web-interface/control/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+from .models import TiberiusRobot
 # Create your views here.
+def index(request):
+    tib = TiberiusRobot.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('web_interface/index.html')
+    context = RequestContext(request, {
+        'tib': tib,
+    })
+    return HttpResponse(template.render(context))
