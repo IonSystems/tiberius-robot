3df3978b59504a1f2b5f8b62056aa0c30127a156
Cameron A. Craig <camieac@gmail.com>
2015-10-04 13:52:44 +0100

    Got login form working.

--- modules/web-interface/users/views.py
+++ modules/web-interface/users/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
+from users.forms import LoginForm
+from django.contrib import auth
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -10,3 +13,30 @@ def index(request):
         'tib': tib,
     })
     return HttpResponse(template.render(context))
+
+
+def login(request):
+    context = RequestContext(request)
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('dashboard')
+            else:
+                form._errors['__all__'] = form.error_class(["Incorrect Username or Password"])
+
+    return render_to_response('login.html', {"form": form}, context)
+
+
+def logout(request):
+    # TODO make logout POST only with js
+    if request.method == "POST":
+        auth.logout(request)
+        return HttpResponse("success")
