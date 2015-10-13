bef7c90ec9ab3bbe43710d7dce1a4893ce238bdc
Cameron A. Craig <camieac@gmail.com>
2015-10-13 10:28:59 +0000

    Added more prints for GPGGA data.

--- modules/control/gps20.py
+++ modules/control/gps20.py
@@ -10,30 +10,50 @@ class GlobalPositioningSystem:
 		self.logger = logging.getLogger('tiberius.control.GlobalPositioningSystem')
 		self.logger.info('Creating an instance of GlobalPositioningSystem')
 		self.ser = serial.Serial(self.port, self.baud, timeout = 1)
-		self.gpgga = nmea.GPGGA()
+		self.__gpgga = nmea.GPGGA()
 		self.debug = debug
 
 		#if(self.s.isOpen()):
 			#self.logger.info('GPS Serial port in open on ', port
 
-	def __fetch_raw_data(self):
+	def __fetch_gpgga_data(self):
 		self.ser.open()
-		data = self.ser.readline()
+		data = ''
+		timeout = 0
+		while(not 'GPGGA' in data or timeout > 20):
+			data = self.ser.readline()
+			timeout += 1
 		self.ser.close()
 		return data
 
 	def __parse_data(self, data):
-		return self.gpgga.parse(data)
+		return self.__gpgga.parse(data)
 
 	def update(self):
-		self.__parse_data(self.__fetch_raw_data())
+		raw = self.__fetch_gpgga_data()
+		if self.debug:
+			self.logger.debug(raw)
+		self.__parse_data(raw)
 
 	def print_data(self):
-		print 'Latitude: ', self.gpgga.latitude
-		print 'Longitude: ', self.gpgga.longitude
-		#print 'Altitude: ', self.gpgga.altitude
+		print 'Timestam: ', self.__gpgga.timestamp
+		print 'Latitude: ', self.__gpgga.latitude
+		print 'Latitude Direction: ', self.__gpgga.lat_direction
+		print 'Longitude: ', self.__gpgga.longitude
+		print 'Longitude Direction: ', self.__gpgga.lon_direction
+		print 'GPS Quality Indicator: ', self.__gpgga.gps_qual
+		print 'Number of Satillites: ', self.__gpgga.num_sats
+		print 'Horizontal Dilution of Precision: ', self.__gpgga.horizontal_dil
+		print 'Antenna altitude above sea level (mean): ', self.__gpgga.antenna_altitude
+		print 'Units of altitude (meters):', self.__gpgga.altitude_units
+		print 'Geoidal Seperation: ', self.__gpgga.geo_sep
+		print 'Units of Geoidal Seperation: ', self.__gpgga.geo_sep_units
+		#print 'Age of Differential GPS Data (sec): ', self.__gpgga.age_gps_data
+		#print 'Differential Reference Station ID: ', self.__gpgga.ref_station_id
 
 
+	def get_data_object(self):
+		return self.__gpgga
 #For testing
 import time
 if __name__ == "__main__":
