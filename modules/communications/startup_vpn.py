b3dcb85d8b32a636fbe83be014160bc127f72aa5
Cameron A. Craig <camieac@gmail.com>
2015-10-19 16:46:58 +0100

    Another fix for startup script.

--- modules/communications/startup_vpn.py
+++ modules/communications/startup_vpn.py
@@ -2,5 +2,12 @@ import subprocess
 import time
 
 if __name__ == "__main__":
-    subprocess.check_output(["pon Server && route add -net 10.113.211.0 netmask 255.255.255.0 dev ppp0"])
-    
+    print 'Running 'pon Server'
+    r1 = subprocess.check_output(["pon Server"])
+    print 'Waiting 10 seconds ...'
+    time.sleep(10)
+    print 'Running route add -net 10.113.211.0 netmask 255.255.255.0 dev ppp0'
+    r2 = subprocess.check_output(["route add -net 10.113.211.0 netmask 255.255.255.0 dev ppp0"])
+    print 'Results: '
+    print r1
+    print r2
