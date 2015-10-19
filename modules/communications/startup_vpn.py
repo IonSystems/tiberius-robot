f573baf21fd88a0b1cf632fa1663f37131a468d5
Cameron A. Craig <camieac@gmail.com>
2015-10-19 16:42:40 +0100

    Fix for startup script.

--- modules/communications/startup_vpn.py
+++ modules/communications/startup_vpn.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
 
 if __name__ == "__main__":
-    subprocess.check_output(["echo", "Hello World!"])
+    subprocess.check_output(["pon Server && route add -net 10.113.211.0 netmask 255.255.255.0 dev ppp0"])
+    
