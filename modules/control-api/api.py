b0136207368da2779dad402a8082a5dd1a1c5a93
Cameron A. Craig <camieac@gmail.com>
2015-12-16 19:32:43 +0000

    Added basic motor control to API.

--- modules/control-api/api.py
+++ modules/control-api/api.py
@@ -1,10 +1,16 @@
 import falcon
+import sys
 
 import sensors
 import motors
 
+# #Import control module
+sys.path.insert(0,'../control')
+from actuators import Motor
+m = Motor()
 api = application = falcon.API(media_type='application/json; charset=utf-8')
 
 sensors = sensors.SensorResource()
+motors = motors.MotorResource(m)
 api.add_route('/sensors', sensors)
 api.add_route('/motors', motors)
--- modules/control-api/motors.py
+++ modules/control-api/motors.py
@@ -1,19 +1,44 @@
-
 import falcon
-
-#Import motor part of control module
-sys.path.insert(0,'../control')
-from control import Control.motors as motors
+import sys
 
 '''
     Controls motor speed, direction, steering angle.
 '''
 class MotorResource(object):
+    def __init__(self, motor_control):
+            self.motor_control = motor_control
 
+    #@falcon.before(validate_params(req, resp, resource, params))
     def on_get(self, req, resp):
-        if(req.size() > 0):
-            print req
-            if(req == ''):
 
-        resp.body = '{"data": "Hello world!"}'
+        #Basic commands used for manual control
+        if('forward' in req.params):
+            speed = req.params['forward']
+            self.motor_control.setSpeedPercent(speed)
+            self.motor_control.moveForward()
+            resp.body = '{"status":{"motors": "forward"}}'
+        elif('backward' in req.params):
+            speed = req.params['backward']
+            self.motor_control.setSpeedPercent(speed)
+            self.motor_control.moveForward()
+            resp.body = '{"status":{"motors": "backward"}}'
+        if('left' in req.params):
+            speed = req.params['left']
+            self.motor_control.setSpeedPercent(speed)
+            motor_control.turnLeft()
+            resp.body = '{"status":{"motors": "left"}}'
+        elif('right' in req.params):
+            speed = req.params['right']
+            self.motor_control.setSpeedPercent(speed)
+            self.motor_control.turnRight()
+            resp.body = '{"status":{"motors": "right"}}'
+        if('stop' in req.params):
+            self.motor_control.stop()
+            resp.body = '{"status":{"motors": "stop"}}'
+
         resp.status = falcon.HTTP_200
+
+def validate_params(req, resp, resource, params):
+    if req.content_type not in ALLOWED_IMAGE_TYPES:
+        msg = 'Image type not allowed. Must be PNG, JPEG, or GIF'
+        raise falcon.HTTPBadRequest('Bad request', msg)
