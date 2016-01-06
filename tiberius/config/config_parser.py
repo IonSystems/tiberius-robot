b1efc612f5aeb0fae6b63853e16c43432decc3a0
Cameron A. Craig <camieac@gmail.com>
2016-01-06 20:52:11 +0000

    Issue #20, added configuration file and supporting accessor functions. Removed old settings database. Includes tests.

---
+++ tiberius/config/config_parser.py
@@ -0,0 +1,95 @@
+import ConfigParser
+
+class TiberiusConfigParser():
+	def __init__(self):
+		self.parser = ConfigParser.ConfigParser()
+		self.parser.read('/etc/tiberius/tiberius_conf.conf')
+
+	'''******************************************
+		Constant values
+	******************************************'''
+	INSTALLED = 'installed'
+	LIDAR_SECTION = 'lidar'
+	ULTRASONICS_SECTION = 'ultrasonics'
+	STEERING_SECTION = 'steering'
+	POWER_SECTION = 'power'
+	NETWORKING_SECTION = 'networking'
+	KINECT_SECTION = 'kinect'
+	ROBOT_ARM_SECTION = 'arm'
+
+	'''******************************************
+		LIDAR
+	******************************************'''
+	def isLidarEnabled(self):
+		return self.parser.getboolean(self.LIDAR_SECTION, 'installed')
+
+	'''******************************************
+		Ultrasonics
+	******************************************'''
+
+	def getUltrasonicFrontCentreAddress(self):
+		addr = self.parser.get(self.ULTRASONICS_SECTION, 'front_centre')
+		return int(addr)
+
+	def getUltrasonicFrontLeftAddress(self):
+		addr = self.parser.get(self.ULTRASONICS_SECTION, 'front_left')
+		return int(addr)
+
+	def getUltrasonicFrontRightAddress(self):
+		addr = self.parser.get(self.ULTRASONICS_SECTION, 'front_right')
+		return int(addr)
+
+	def getUltrasonicRearCentreAddress(self):
+		addr = self.parser.get(self.ULTRASONICS_SECTION, 'rear_centre')
+		return int(addr)
+
+	def getUltrasonicRearLeftAddress(self):
+		addr = self.parser.get(self.ULTRASONICS_SECTION, 'rear_left')
+		return int(addr)
+
+	def getUltrasonicRearRightAddress(self):
+		addr = self.parser.get(self.ULTRASONICS_SECTION, 'rear_right')
+		return int(addr)
+
+	'''******************************************
+		Networking
+	******************************************'''
+	def getIPAddress(self):
+		ipa = self.parser.get(self.NETWORKING_SECTION, 'ip_address')
+		return ipa
+
+	'''
+	A setter method is required to deal with IP address changes if using DHCP.
+	'''
+	def setIPAddress(self, ip_address):
+		result = self.parser.set(self.NETWORKING_SECTION, 'ip_address', ip_address)
+		return result
+
+	def getName(self):
+		name = self.parser.get(self.NETWORKING_SECTION, 'name')
+		return name
+
+	'''******************************************
+		Steering
+	******************************************'''
+
+	def getSteeringType(self):
+		type = self.parser.get(self.STEERING_SECTION, 'type')
+		return type
+
+	'''******************************************
+		Power
+	******************************************'''
+
+	def getBatteryCapacity(self):
+		capacity = self.parser.get(self.POWER_SECTION, 'capacity')
+		return capacity
+
+	def getBatteryChemistry(self):
+		chemistry = self.parser.get(self.POWER_SECTION, 'chemistry')
+		return chemistry
+
+if __name__ == "__main__":
+	c = TiberiusConfigParser()
+	ip_address = c.getIPAddress()
+	print ip_address
--- tiberius/testing/test_configuration.py
+++ tiberius/testing/test_configuration.py
@@ -1,7 +1,76 @@
 import unittest
 import sys
-from tiberius.settings.config_parser import TiberiusConfigParser
+import socket
+from tiberius.config.config_parser import TiberiusConfigParser
 
-class IPAddress(unittest.TestCase):
+config = TiberiusConfigParser()
+
+class ChangeIPAddress(unittest.TestCase):
     def runTest(self):
+        original = config.getIPAddress()
+        new = '10.8.8.21'
+        config.setIPAddress(new)
+        check = config.getIPAddress()
+        self.assertEquals(new, check)
         return
+
+class ValidIPAddress(unittest.TestCase):
+    def runTest(self):
+        ip_address = config.getIPAddress()
+        try:
+            socket.inet_aton(ip_address)
+        except:
+            self.fail("Invalid IP Address")
+        return
+
+class GetUltrasonicAddresses(unittest.TestCase):
+    def runTest(self):
+         rl = config.getUltrasonicRearLeftAddress()
+         rr = config.getUltrasonicRearRightAddress()
+         rc = config.getUltrasonicRearCentreAddress()
+         fl = config.getUltrasonicFrontLeftAddress()
+         fr = config.getUltrasonicFrontRightAddress()
+         fc = config.getUltrasonicFrontCentreAddress()
+
+         minAddress = 0
+         maxAddress = 255
+
+         self.assertTrue(isinstance(rl,(int, long)))
+         self.assertTrue(isinstance(rr,(int, long)))
+         self.assertTrue(isinstance(rc,(int, long)))
+         self.assertTrue(isinstance(fl,(int, long)))
+         self.assertTrue(isinstance(fr,(int, long)))
+         self.assertTrue(isinstance(fc,(int, long)))
+
+class GetSteeringType(unittest.TestCase):
+    def runTest(self):
+        st = config.getSteeringType()
+        self.assertTrue(st == "Skid" or st == "Articulated" )
+
+class CheckLidarValue(unittest.TestCase):
+    def runTest(self):
+        val = config.isLidarEnabled()
+        self.assertTrue(isinstance(val, (bool)))
+
+class CheckUnitName(unittest.TestCase):
+    def runTest(self):
+        val = config.getName()
+        self.assertTrue(isinstance(val, (str)))
+        #Max length based on specified max SSID length
+        self.assertTrue(0 <= len(val) <= 32)
+
+class CheckBatteryCapacity(unittest.TestCase):
+    def runTest(self):
+        val = long(config.getBatteryCapacity())
+        self.assertTrue(isinstance(val, (int, long)))
+        self.assertTrue(100 <= val <= 10000)
+
+class CheckBatteryChemistry(unittest.TestCase):
+    def runTest(self):
+        val = config.getBatteryChemistry()
+        self.assertTrue(val == "LiFe" or val == "LiPo" or val == "Lead")
+
+class CheckLidarValue(unittest.TestCase):
+    def runTest(self):
+        val = config.isLidarEnabled()
+        self.assertTrue(isinstance(val, (bool)))
