70e7cc6d7470e3c01dbd2ac00ff6c7aec2de7f8d
Cameron A. Craig <camieac@gmail.com>
2016-01-10 19:37:39 +0000

    Issue #8, added script to install smbus-cffi dependencies, to prevent the installation failing.

--- setup.py
+++ setup.py
@@ -1,6 +1,66 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.command.install import install
+from subprocess import check_output, CalledProcessError
+
+class PostInstallDependencies(install):
+	def run(self):
+		print "Checking for missing packages"
+		self.install_if_missing("build-essential")
+		self.install_if_missing("libi2c-dev")
+		self.install_if_missing("i2c-tools")
+		self.install_if_missing("python-dev")
+
+		print "Removing I2C from blacklist on Raspberry Pi"
+		blacklist_dir = "/etc/modprobe.d/raspi-blacklist.conf"
+		enable_command = "sed -i 's/blacklist i2c-bcm2708/#blacklist i2c-bcm2708/g' " + blacklist_dir
+		check_output(enable_command, shell = True)
+
+		print "Adding i2c-dev and i2c-bcm2708 to enabled modules"
+		modules_dir = "/etc/modules"
+		if(self.is_text_found("i2c-dev", modules_dir)):
+			print "i2c-dev already enabled"
+		else:
+			print "Enabling i2c-dev"
+			enable_command = "echo 'i2c-dev' | sudo tee -a " + modules_dir
+			check_output(enable_command, shell = True)
+
+		if(self.is_text_found("i2c-bcm2708", modules_dir)):
+			print "i2c-bcm2708 already enabled"
+		else:
+			print "Enabling i2c-bcm2708"
+			enable_command = "echo 'i2c-bcm2708' | sudo tee -a " + modules_dir
+			check_output(enable_command, shell = True)
+
+		#Continue with the rest of the installation process.
+		install.run(self)
+
+	def is_text_found(self, text, file):
+		try:
+			if(text in check_output("grep -r " + text + " " + file, shell = True)):
+				return True
+			else:
+				return False
+		except CalledProcessError:
+			return False 
+
+	def is_package_installed(self, package_name):
+		result = check_output("dpkg-query -l " + package_name, shell = True)
+		if("no packages found" in result):
+			return False
+		else:
+			return True
+
+	def install_package(self, package_name):
+		check_output("sudo apt-get --force-yes install " + package_name)
+
+	def install_if_missing(self, package_name):
+		if not (self.is_package_installed(package_name)):
+			print "Installing " + package_name
+			self.install_package(package_name)
+		else:
+			print package_name + " already installed"
 
 setup(name='Tiberius',
       version='1.0',
@@ -15,5 +75,6 @@ setup(name='Tiberius',
                             #('/etc/tiberius', ['tiberius/database/polyhedra_databse.db'])
                         ],
       platforms=['Raspberry Pi 2', 'Raspberry Pi 1'],
-      install_requires=['smbus-cffi'],
+      install_requires=['enum34', 'smbus-cffi'],
+      cmdclass={'install':PostInstallDependencies},
      )
