2d7c856122dbb7bc9b1eda4953b9fe27a5c4ce3a
Cameron A. Craig <camieac@gmail.com>
2016-01-14 12:52:49 +0000

    Started robot modification form.

--- tiberius/web-interface/fleet/views.py
+++ tiberius/web-interface/fleet/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.contrib.auth.decorators import login_required
+from django.forms.models import model_to_dict
 
 from .models import Robot
+from .forms import RobotForm
 
 @login_required(login_url='/users/login/')
 def list(request):
@@ -14,3 +16,26 @@ def list(request):
         'fleet': fleet,
     })
     return HttpResponse(template.render(context))
+
+@login_required(login_url='/users/login/')
+def modify(request, id):
+    robot = Robot.objects.get(id = id)
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RobotForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            form.save()
+            return render(request, 'index.html')
+
+        else:
+            return render(request, 'modify.html', {'form': form, 'robot':robot})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = RobotForm(initial = model_to_dict(robot))
+
+    return render(request, 'modify.html', {'form': form, 'robot':robot})
