3758a3ee75a1a0817f409f896696cfadbeec15a8
StuartThain <stuartthain99@gmail.com>
2016-01-22 15:36:34 +0000

    Initial point to point. Only runs check once so not going to go to right location unless tiberius moves correctly!!

--- tiberius/navigation/gps/algorithms.py
+++ tiberius/navigation/gps/algorithms.py
@@ -2,6 +2,7 @@ import math
 import null as null
 
 from tiberius.control.gps20 import GlobalPositioningSystem
+from tiberius.control.control import Control
 
 class Algorithms:
 	'''
@@ -10,8 +11,12 @@ class Algorithms:
 
 	'''
 
+
+
 	def __init__(self):
 		self.gps = GlobalPositioningSystem()
+		self.control = Control()
+		self.SPEED = 0.5  # meters/second
 
 	# get the current location of tiberius
 	def getLocation(self):
@@ -35,11 +40,24 @@ class Algorithms:
 
 		return heading
 
+	def getDistance(self, curlocation, destination):
+		r = 6371000  # radius of the earth in meters
+		longdis = curlocation[0] - destination[0]
+		latdis = curlocation[1] - destination[1]
+		a = math.sin(latdis / 2) * math.sin(latdis / 2) + \
+			math.cos(curlocation[1]) * math.cos(destination[1]) * \
+			math.sin(longdis / 2) * math.sin(longdis / 2)
+		c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+		distance = r * c
+		return distance
+
 	# move from current location to a given destination (a longitude and latitude)
 	def pointToPoint(self, destination):
 
 		curlocation = self.getLocation()
 		heading = self.getHeading(curlocation, destination)
-		
-
-		return null
+		distence = self.getDistance(curlocation, destination)
+		time = distence / (self.SPEED / 2)
+		self.control.turnTo(heading)
+		self.control.driveStraight(50, time)
+		print "The task is complete"
