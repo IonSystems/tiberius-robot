a2e80d83dbf7858123256b612d7181570e232934
Euan Mutch <euan.mutch@gmail.com>
2016-01-25 19:10:10 +0000

    Fixing some things

--- tiberius/navigation/gps/algorithms.py
+++ tiberius/navigation/gps/algorithms.py
@@ -1,59 +1,55 @@
 import math
-import null as null
 
 from tiberius.control.gps20 import GlobalPositioningSystem
 from tiberius.control.control import Control
 
+
 class Algorithms:
-	'''
+    '''
 		Set of algorithms that control tiberius using the gps and compass
 
 
 	'''
 
+    def __init__(self):
+        self.gps = GlobalPositioningSystem()
+        self.control = Control()
+        self.SPEED = 0.5  # meters/second
 
+    # get the current location of tiberius
+    def getLocation(self):
+        location = [self.gps.longitude, self.gps.latitude]
+        return location
 
-	def __init__(self):
-		self.gps = GlobalPositioningSystem()
-		self.control = Control()
-		self.SPEED = 0.5  # meters/second
+    def getHeading(self, curlocation, destination):
+        curlongitude = float(curlocation[0])
+        curlatitude = float(curlocation[1])
+        deslongitude = float(destination[0])
+        deslatitude = float(destination[1])
 
-	# get the current location of tiberius
-	def getLocation(self):
+        y = math.sin(deslongitude - curlongitude) * math.cos(deslatitude)
+        x = math.cos(curlatitude) * math.sin(deslatitude) - \
+            math.sin(curlatitude) * math.cos(deslatitude) * math.cos(deslongitude - curlongitude)
+        heading = math.degrees(math.atan2(y, x) + 360) % 360
 
-		location = []
-		location[0] = self.gps.longitude
-		location[1] = self.gps.latitude
+        return heading
 
-		return location
+    def getDistance(self, curlocation, destination):
+        r = 6371000  # radius of the earth in meters
+        longdis = float(curlocation[0]) - float(destination[0])
+        latdis = float(curlocation[1]) - float(destination[1])
+        a = math.sin(latdis / 2) * math.sin(latdis / 2) + \
+            math.cos(float(curlocation[1])) * math.cos(float(destination[1])) * \
+            math.sin(longdis / 2) * math.sin(longdis / 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = r * c
+        return distance
 
-	def getHeading(self, curlocation, destination):
-		curlongitude = curlocation[0]
-		curlatitude = curlocation[1]
-		deslongitude = destination[0]
-		deslatitude = destination[1]
-
-		y = math.sin(deslongitude - curlongitude) * math.cos(deslatitude)
-		x = math.cos(curlatitude) * math.sin(deslatitude) - \
-			math.sin(curlatitude) * math.cos(deslatitude) * math.cos(deslongitude - curlongitude)
-		heading = math.degrees(math.atan2(y, x) + 360) % 360
-
-		return heading
-
-	def getDistance(self, curlocation, destination):
-		r = 6371000  # radius of the earth in meters
-		longdis = curlocation[0] - destination[0]
-		latdis = curlocation[1] - destination[1]
-		a = math.sin(latdis / 2) * math.sin(latdis / 2) + \
-			math.cos(curlocation[1]) * math.cos(destination[1]) * \
-			math.sin(longdis / 2) * math.sin(longdis / 2)
-		c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-		distance = r * c
-		return distance
-
-	# move from current location to a given destination (a longitude and latitude)
-	def pointToPoint(self, destination, checkdistance=1, speedpercent=50):
-
+    # move from current location to a given destination (a longitude and latitude)
+    def pointToPoint(self, destination, checkdistance=1, speedpercent=50):
+		if self.gps.longitude == None:
+		    print 'NO FIX'
+		    return
 		speed = 100 / speedpercent
 		curlocation = self.getLocation()
 		heading = self.getHeading(curlocation, destination)
@@ -65,11 +61,10 @@ class Algorithms:
 			distance -= checkdistance
 			curlocation = self.getLocation()
 			newheading = self.getHeading(curlocation, destination)
-			if not(heading + 5 < newheading < 5 - heading):
+			if not (heading + 5 < newheading < 5 - heading):
 				heading = newheading
-				distance = self.getDistance(curlocation, destination)
-				self.control.turnTo(heading)
-
+                distance = self.getDistance(curlocation, destination)
+                self.control.turnTo(heading)
 		curlocation = self.getLocation()
 		heading = self.getHeading(curlocation, destination)
 		distance = self.getDistance(curlocation, destination)
