6d3b0cd21998b79761fc9da8f127c82f8e52b015
Cameron A. Craig <camieac@gmail.com>
2016-02-10 13:18:17 +0000

    Now generates instance of all Tasks.

--- tiberius/control_api/task.py
+++ tiberius/control_api/task.py
@@ -7,15 +7,25 @@ class Task(object):
     '''
     An abstract Task that is implemented in order to build usable tasks for Tiberius to run during a mission.
     '''
-    @abc.abstractmethod
     def __init__(self):
-        self.task_name = task_name
-        self.task_id = task_id
-        self.task_description = task_description
+
+        #@abs.abstractproperty
+        self.task_name = None
+        self.task_id = None
+        self.task_description = None
 
         self.task_complete = self.TaskState.STOPPED
 
         self.control = Control()
+
+    def __str__(self):
+        return str({'name': self.task_name,
+                    'task_id': self.task_id})
+
+    def setupTask(self, task_name, task_id, task_description):
+        self.task_name = task_name
+        self.task_id = task_id
+        self.task_description = task_description
 
     @abc.abstractmethod
     def runTask(self):
--- tiberius/control_api/task_controller.py
+++ tiberius/control_api/task_controller.py
@@ -4,6 +4,7 @@ from tiberius.logger import logger
 import logging
 from tiberius.control_api.task import *
 from tiberius.control_api.tasks.driving_tasks import *
+import threading
 
 '''
     Controls motor speed, direction, steering angle.
@@ -15,6 +16,8 @@ class TaskControllerResource(object):
     def __init__(self):
         self.logger = logging.getLogger(
             'tiberius.control_api.TaskControllerResource')
+
+        # Find all the tasks avaibale and make them accessible here.
         self.tasks = self.find_tasks()
         self.current_task_id = None
 
@@ -23,18 +26,24 @@ class TaskControllerResource(object):
         resp.body = '{"status":{"motors": "forward"}}'
 
     def find_tasks(self):
-        tasks = set()
-        for task in Task.__subclasses__():
-            if task not in tasks:
-                tasks.add(task)
-            print task
+        # tasks = set()
+        # cls() for cls in Task.__subclasses__():
+        #     if cls not in tasks:
+        #         tasks.add(cls)
+        # return tasks
+        tasks = [cls() for cls in Task.__subclasses__()]
         return tasks
 
     def run_task(self, task_id):
         for task in self.tasks:
-            print task
-            if(task.task_id == task_id):
-                task.runTask()
+            print str(task)
+            if task.task_id == task_id:
+                self.logger.info("Running Task: %s", str(task))
+                #thread.start_new_thread( task.runTask() )
+                #task.runTask()
+
+            # if(task.__class__.task_id == task_id):
+            #    task.runTask()
 
 
 def validate_params(req, resp, resource, params):
