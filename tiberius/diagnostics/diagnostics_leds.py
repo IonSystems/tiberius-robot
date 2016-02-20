6b671f13edf7fbf1f31fbe8caf76298da42f00cc
Euan Mutch <euan.mutch@gmail.com>
2016-02-20 15:19:04 +0000

    Added diagnostics_leds

---
+++ tiberius/diagnostics/diagnostics_leds.py
@@ -0,0 +1,54 @@
+import serial
+from tiberius.utils import detection
+import time
+
+
+class diagnostics_leds:
+    if detection.detect_windows():
+        port = 'COM8'
+    else:
+        port = '/dev/ttyACM0'
+    baud = 9600
+
+    def __init__(self, debug=False):
+        self.ser = serial.Serial(self.port, self.baud, timeout=1)
+        try:
+            self.ser.open()
+
+        except:
+            print "Port already open or in use"
+        time.sleep(3)
+
+    def setLEDs(self, led0, led1, led2, led3, led4, led5, led6, led7):
+        if not self.ser.is_open:
+            try:
+                self.ser.open()
+            except:
+                print "Port already open or in use"
+            time.sleep(3)
+        if self.ser.is_open:
+            data = "sd" + str(led0) + "d" + str(led1) + "d" + str(led2) + "d" + str(led3) + "d" + str(led4) + "d" + str(
+                    led5) + "d" + str(led6) + "d" + str(led7)
+
+            self.ser.write(data)
+
+
+if __name__ == "__main__":
+    # from tiberius.diagnostics.diagnostics_leds import diagnostics_leds
+    from random import randint
+
+    leds = diagnostics_leds()
+
+    l1, l2, l3, l4, l5, l6, l7, l8 = 0, 0, 0, 0, 0, 0, 0, 0
+
+    while (True):
+        leds.setLEDs(l1, l2, l3, l4, l5, l6, l7, l8)
+        l1 = randint(0, 5)
+        l2 = randint(0, 5)
+        l3 = randint(0, 5)
+        l4 = randint(0, 5)
+        l5 = randint(0, 5)
+        l6 = randint(0, 5)
+        l7 = randint(0, 5)
+        l8 = randint(0, 5)
+        time.sleep(0.1)
