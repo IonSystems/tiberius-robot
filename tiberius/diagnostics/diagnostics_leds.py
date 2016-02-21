8615ad307b710bde1978ea539827ec14521153b6
Andrew Rigg <andrewjrigg@gmail.com>
2016-02-21 16:00:45 +0000

    diagnostics

--- tiberius/diagnostics/diagnostics_leds.py
+++ tiberius/diagnostics/diagnostics_leds.py
@@ -12,21 +12,16 @@ class diagnostics_leds:
 
     def __init__(self, debug=False):
         self.ser = serial.Serial(self.port, self.baud, timeout=1)
-        try:
-            self.ser.open()
 
-        except:
-            print "Port already open or in use"
-        time.sleep(3)
 
     def setLEDs(self, led0=-1, led1=-1, led2=-1, led3=-1, led4=-1, led5=-1, led6=-1, led7=-1):
-        if not self.ser.is_open:
+        if not self.ser.isOpen():
             try:
                 self.ser.open()
             except:
-                print "Port already open or in use"
-            time.sleep(3)
-        if self.ser.is_open:
+                print "Port already open"
+            time.sleep(10)
+        if self.ser.isOpen():
             data = "sd" + str(led0) + "d" + str(led1) + "d" + str(led2) + "d" + str(led3) + "d" + str(led4) + "d" + str(
                 led5) + "d" + str(led6) + "d" + str(led7)
 
