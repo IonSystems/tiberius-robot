027a715dcab3d8dd2e83ab40aaa0b48bccdf213a
StuartThain <stuartthain99@gmail.com>
2016-03-31 18:23:07 +0100

    standard dev module is nearly done and is beginning to shape the other two modules as it goes along

--- verilog/standard_deviation/standard_deviation_top.sv
+++ verilog/standard_deviation/standard_deviation_top.sv
@@ -21,9 +21,10 @@ input logic 				clk,
 input logic 				rstn,
 input logic 	[32:0] 	epack,
 input logic 				data_recieved,	//data has been collected by the standard_deviation module
+input logic					data_complete, //comes from standard dev
 
 output logic 	[15:0] 	pixel_packs [3392],
-output logic 				data_sent		//lets the standard_deviation module know there is data ready
+output logic				data_ready //lets the standard_deviation module know there is data ready
 
 );
 
@@ -40,7 +41,7 @@ always@(posedge clk or negedge rstn)
 		if(rstn == 'b0)
 			begin
 			pixel_packs 		<= '{default:'b0};
-			data_sent 			<= 'b0;
+			data_ready 			<= 'b0;
 			state 				<= signal_wait;
 			chunck_data 		<= '{default:'b0};
 			data_count 			<= 'd0;
@@ -59,7 +60,15 @@ always@(posedge clk or negedge rstn)
 						end
 					latch_data:
 						begin
-						
+						if(data_complete == 'b0) //make sure the last data set is done
+							begin
+							pixel_packs <= chunck_data;
+							data_ready <= 'b1;
+							end
+						else
+							begin
+							state <= latch_data;
+							end
 						end
 					default: state <= signal_wait;
 				endcase
@@ -73,16 +82,20 @@ module standard_deviation(
 
 input logic 				clk,
 input logic 				rstn,
-input logic 				data_sent,	//data ready to be collected
+input logic 				data_ready,	//data ready to be collected
+input logic					data_sent, //data is sent to the PC
 input logic 	[15:0] 	pixel_pack [3392],
 
 output logic 	[15:0] 	sd_out,
-output logic 				data_recieved	//data collected
+output logic 				data_recieved,	//data collected
+output logic				data_complete	//goes to data collector
 
 );
 
 typedef enum { data_wait, sum_data, calc, calc_sd} states;
 states 					state;
+typedef enum { send, hold, received} sending_states;
+sending_states			sending_state;
 
 logic [31:0] 			sum, vsum;
 logic [31:0] 			mean, variance, sd;
@@ -110,13 +123,22 @@ always@(posedge clk)
 				variance 		<= 'd0;
 				sd 				<= 'd0;
 				sum_count		<= 'd0;
+				state				<= data_wait;
+				sending_state	<= send;
 			end
 		else
 			begin
 				case(state)
 					data_wait:
 						begin
-						
+						if(!data_ready)
+							begin
+							state <= data_wait;
+							end
+						else
+							begin
+							state <= sum_data;
+							end
 						end
 					sum_data:
 						begin
@@ -154,6 +176,48 @@ always@(posedge clk)
 							sqrt_count++;
 							state <= calc_sd;
 							end
+						end
+					send_data:
+						begin
+							case(sending_state)
+								send:
+									begin
+									sd_out <= sd;
+									//could put mean here
+									//could put variance here
+									sending_state <= hold;
+									state <= send_data;
+									end
+								hold:
+									begin
+										if(data_sent)
+											begin
+											sending_state <= received;
+											state <= send_data;
+											end
+										else
+											begin
+											sending_state <= hold;
+											state <= send_data;
+											end
+									end
+								received:
+									begin
+										//get signal that data is sent
+										if(data_received)
+											begin
+											//set done signal so next one can start e.g data_complete <= 'b1
+											sending_state <= send;
+											state <= data_wait;
+											end
+										else
+											begin
+											sending_state <= received;
+											state <= send_data;
+											end
+									end
+								default: state <= received;
+							endcase
 						end
 					default: state <= data_wait;
 				endcase
