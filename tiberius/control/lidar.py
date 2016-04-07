f4def590eeee0d10fd28fd3193da1ccbaa0c3354
Tiberius 3 <tiberius.robot@gmail.com>
2016-04-07 17:40:27 +0000

    minor changes

--- tiberius/control/lidar.py
+++ tiberius/control/lidar.py
@@ -6,16 +6,18 @@ import json
 
 class RoboPeakLidar:
 
-    def get_lidar_data():
+    def get_lidar_data(self):
         args = ("/home/pi/git/tiberius-robot/tiberius/autonomy/readlidar/readlidar")
         popen = subprocess.Popen(args, stdout=subprocess.PIPE)
         popen.wait()
         output = popen.stdout.read()
-        return format_data(output)
+        return self.format_data(output)
 
-    def format_data(data):
+    def format_data(self, data):
+        print data # debug
         return json.loads(data)
 
 
 if __name__ == "__main__":
-    print get_lidar_data()
+    l = RoboPeakLidar()
+    print l.get_lidar_data()
