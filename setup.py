f73c3bb386e5c900d5259b6e52b106bf3b762154
apg30 <aidang123@googlemail.com>
2016-04-09 18:10:26 +0100

    old setup

--- setup.py
+++ setup.py
@@ -78,13 +78,13 @@ class PostInstallDependencies(install):
     '''
     def run(self):
         if is_windows():
-            print "Installing on Windows."
+        print "Installing on Windows."
         elif is_pi():
-            print "Installing on Raspberry Pi."
+        print "Installing on Raspberry Pi."
             self.install_deps_pi()
             self.install_poly_pi()
         elif is_linux():
-            print "Installing on Linux."
+        print "Installing on Linux."
             self.install_deps_linux()
             self.install_poly_linux()
             self.create_lidar_executable()
@@ -104,7 +104,8 @@ class PostInstallDependencies(install):
     self.enable_modules_i2c()
 
     def create_lidar_executable(self):
-        binary = check_output("cd ~/git/tiberius-robot/tiberius/autonomy/readlidar && g++ -pthread -lrt rplidar_driver.cpp thread.cpp net_serial.cpp timer.cpp readlidar.cpp -o readlidar", shell=True)
+        move = check_output("cd ~/git/tiberius-robot/tiberius/autonomy/readlidar", shell=True)
+        binary = check_output("g++ -pthread -lrt rplidar_driver.cpp thread.cpp net_serial.cpp timer.cpp readlidar.cpp -o readlidar", shell=True)
         print "creating lidar executable"
 
     def install_deps_linux(self):
