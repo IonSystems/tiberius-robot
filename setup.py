cb94f0e8333971b00c7da1c408bb6e10caee2518
Tiberius 3 <tiberius.robot@gmail.com>
2016-04-11 12:19:22 +0000

    Added back PostInstallDependencies.

--- setup.py
+++ setup.py
@@ -356,7 +356,7 @@ setup(name='Tiberius',
           (odbc_directory, ['vendor/polyhedra-driver/odbcinst.ini']),
       ],
       platforms=['Raspberry Pi 2', 'Raspberry Pi 1'],
-      install_requires=requirements
-      #cmdclass={
-      # 'install': PostInstallDependencies},
+      install_requires=requirements,
+      cmdclass={
+       'install': PostInstallDependencies},
       )
