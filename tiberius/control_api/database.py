0c24cb03bcce81409937387979da4d94f8d19cce
Cameron A. Craig <camieac@gmail.com>
2016-05-11 00:14:20 +0100

    Started api database query.

--- tiberius/control_api/database.py
+++ tiberius/control_api/database.py
@@ -2,7 +2,10 @@
 import falcon
 from tiberius.database_wrapper.polyhedra_database import PolyhedraDatabase
 
-
+class DatabaseCommands:
+    QUERY = "query"
+    INSERT = "insert"
+    
 class DatabaseResource(object):
     '''
         Allows access to the in-memory database on Tiberius.
@@ -11,5 +14,9 @@ class DatabaseResource(object):
         self.database = PolyhedraDatabase('api_connection')
 
     def on_post(self, req, resp):
+
+        if DatabaseCommands.QUERY in req.params:
+            table = params['table']
+            columns = params['columns']
         resp.body = '{"data": "Hello world!"}'
         resp.status = falcon.HTTP_200
