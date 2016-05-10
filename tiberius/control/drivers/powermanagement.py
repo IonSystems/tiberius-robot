8f1c43859b615442b5e1300fd405b27a7696192f
aidan <aidang123@gmail.com>
2016-05-10 12:16:22 +0000

    put it into a class and creating functions

--- tiberius/control/drivers/powermanagement.py
+++ tiberius/control/drivers/powermanagement.py
@@ -6,25 +6,31 @@ import json
 
 print "running"
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port='/dev/ttyACM0',
-    baudrate=115200,
-)
+class PowerManagement:
 
-while(1):
-	
-	data = ser.readline() #read(1000)  # read 9999 bytes of data. could use .readline() to get line of data
-	if len(data) > 0:
-        	print 'Got:', data
+	# configure the serial connections (the parameters differs on the device you are connecting to)
+	ser = serial.Serial(
+    	port='/dev/ttyACM0',
+    	baudrate=115200,
+	)
+
+	def getdata(self):
+		data = ser.readline() #read(1000)  # read 9999 bytes of data. could use .readline() to get line of data
 		dictionary = json.loads(data)
-		#print dictionary
-		monitor = dictionary["monitor"]
-		volts = dictionary["volts"]
-		current = dictionary["current"]
-		power = dictionary["power"]
-		time = dictionary["time"]
-		amp_hours = dictionary["amp_hours"]
-		watt_hours = dictionary["watt_hours"]
-
-		print monitor
+		return dictionary
+		
+	def testscript(self):
+		while(1):
+			data = ser.readline() #read(1000)  # read 9999 bytes of data. could use .readline() to get line of data
+			if len(data) > 0:
+        			print 'Got:', data
+				dictionary = json.loads(data)
+				#print dictionary
+				monitor = dictionary["monitor"]
+				volts = dictionary["volts"]
+				current = dictionary["current"]
+				power = dictionary["power"]
+				time = dictionary["time"]
+				amp_hours = dictionary["amp_hours"]
+				watt_hours = dictionary["watt_hours"]
+				print monitor
