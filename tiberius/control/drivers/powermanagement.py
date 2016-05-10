a3b286b3e8d30ab1e004efb6af4e241115f16db5
aidan <aidang123@gmail.com>
2016-05-10 11:45:04 +0000

    collecting power mangement data from mbed

--- tiberius/control/drivers/powermanagement.py
+++ tiberius/control/drivers/powermanagement.py
@@ -7,12 +7,14 @@ print "running"
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-    port='/dev/ttyUSB1',
-    baudrate=9600,
+    port='/dev/ttyACM0',
+    baudrate=115200,
 )
 
-data = ser.read(9999)  # read 9999 bytes of data. could use .readline() to get line of data
-if len(data) > 0:
-        print 'Got:', data
+while(1):
+	print "happening once"
+	data = ser.readline() #read(1000)  # read 9999 bytes of data. could use .readline() to get line of data
+	if len(data) > 0:
+        	print 'Got:', data
 
-#asdasdasd
+
