4f2fb899031ff2db40a1e75b26b3c50b8b4bdcb7
StuartThain <stuartthain99@gmail.com>
2016-05-11 00:30:12 +0100

    array

--- tiberius/diagnostics/external_hardware_controller.py
+++ tiberius/diagnostics/external_hardware_controller.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     l1, l2, l3, l4, l5, l6, l7, l8 = 0, 0, 0, 0, 0, 0, 0, 0
 
     while (True):
-        externalHardwareController.set_hardware({l1, l2, l3, l4, l5, l6, l7, l8})
+        externalHardwareController.set_hardware([l1, l2, l3, l4, l5, l6, l7, l8])
         l1 = randint(0, 5)
         l2 = randint(0, 5)
         l3 = randint(0, 5)
